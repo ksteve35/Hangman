45d7bde4d4e3b1e0169770ef424f9e1f7c7d4e4a
ksteve35 <55467467+ksteve35@users.noreply.github.com>
2020-06-27 17:18:55 -0400

    Made the game completely functional
    The game is able to be played from beginning to end when playing as intended. Still needs some bug fixing and visual changes.

--- Hangman.java
+++ Hangman.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -18,11 +19,38 @@ public class Hangman {
     
     public static Scanner scan = new Scanner(System.in);
     public static String[] words, guessedLetters, gameStatus;
-    public static String wordToGuess;
+    public static String wordToGuess, intro;
     public static int lives = 6;
     
     public static void main(String[] args) {
+        boolean livesLeft = true;
         init();
+        System.out.println(intro);
+        
+        while (livesLeft) {
+            // Main game loop
+            
+            System.out.println("\n\n");
+            for (int i = 0; i < gameStatus.length; i++) {
+                System.out.print(gameStatus[i]);
+            }
+            System.out.println("\nIncorrectly guessed letters:");
+            for (int i = 0; i < guessedLetters.length; i++) {
+                if (guessedLetters[i] != null && !wordToGuess.contains(guessedLetters[i]))
+                    System.out.print(guessedLetters[i] + "\t");
+            }
+            
+            guessALetter();
+            boolean allLettersGuessed = answerCheck();
+            if (lives <= 0 || allLettersGuessed)
+                livesLeft = false;
+        }
+        
+        if (lives <= 0) {
+            System.out.println("Game Over!\nSorry, but you guessed six incorrect letters. You lose.\nThe word was \"" + wordToGuess + "\".");
+        } else {
+            System.out.println("CONGRATULATIONS! You guessed the word! It was \"" + wordToGuess + "\".");
+        }
     }
     
     public static void init() {
@@ -33,17 +61,29 @@ public class Hangman {
         */
         
         words = loadWordsFromFile();
+        // Debugging print statement
         //System.out.println("\n\n\nWord count:\t" + words.length + "\nFirst word:\t" + words[0] + "\nLast word:\t" + words[words.length - 1]);
         
         Random r = new Random();
-        wordToGuess = words[r.nextInt(words.length)];
+        wordToGuess = words[r.nextInt(words.length)].toUpperCase();
+        guessedLetters = new String[1];
         
         gameStatus = new String[wordToGuess.length()];
         for (int i = 0; i < gameStatus.length; i++) {
             gameStatus[i] = "_ ";
         }
         
-        
+        intro = "Welcome to Hangman! The rules are simple:\n"
+                + "Below are blank lines that represent letters\n"
+                + "in a word. Your goal is to guess the letters\n"
+                + "that make up this word before you lose. You\n"
+                + "can guess up to six (6) incorrect letters\n"
+                + "before you cause a Game Over and lose the game.\n"
+                + "Incorrectly guessed letters will appear on the\n"
+                + "screen for reference, and correctly guessed\n"
+                + "letters will replace the blank lines in the\n"
+                + "mysterious word you're guessing. Good luck and\n"
+                + "have fun!\n\n";
         
     }
     
@@ -84,6 +124,78 @@ public class Hangman {
         
         System.out.println("\n\n");
         return retVal;
+    }
+    
+    public static void guessALetter() {
+        /*
+        Infinite loop method that only ends when the player inputs a single
+        letter character as their guess that hasn't already been guessed.
+        */
+        
+        while (true) {
+            System.out.println("\n\nPlease guess a letter, then press [ENTER]:\t");
+            String s = scan.nextLine().trim().toUpperCase();
+            boolean exists = Arrays.asList(guessedLetters).contains(s);
+            if (s.length() == 1 && s.matches("[A-Z]+") && !exists) {
+                
+                char c = s.charAt(0);
+                boolean inWordToGuess = wordToGuess.contains(s);
+                
+                if (inWordToGuess) {
+                    int[] indices = new int[wordToGuess.length()];
+                    for (int i = 0; i < indices.length; i++) {
+                        if (wordToGuess.charAt(i) == c) {
+                            gameStatus[i] = c + " ";
+                        }
+                    }
+                } else {
+                    // If wordToGuess does NOT contain the guessed character,
+                    // the player will lose a life.
+                    lives--;
+                }
+                
+                if (guessedLetters[0] != null) {
+                    String[] alreadyGuessedLetters = new String[guessedLetters.length + 1];
+                
+                    /*
+                    For loop copies String[] guessedLetters into a new String[]
+                    alreadyGuessedLetters and adds the new guess to the new String[].
+                    Then, alreadyGuessedLetters is copied as the new String[]
+                    guessedLetters.
+                    */
+
+                    for (int i = 0; i < guessedLetters.length; i++) {
+                        alreadyGuessedLetters[i] = new String(guessedLetters[i]);
+                        if (i + 1 == guessedLetters.length)
+                            alreadyGuessedLetters[i + 1] = new String(s);
+                    }
+                    guessedLetters = alreadyGuessedLetters.clone();
+
+                    break;
+                } else {
+                    guessedLetters[0] = s;
+                    break;
+                }
+                
+            } else if (s.length() != 1) {
+                System.out.println("Guess cannot be shorter or longer than a single character.");
+            } else if (!s.matches("[A-Z]+")) {
+                System.out.println("Guess must be a letter.");
+            } else if (exists) {
+                System.out.println("You have already guessed the letter '" + s + "'");
+            } else {
+                System.out.println("Something went wrong with your guess...");
+            }
+            
+        }
+    }
+
+    public static boolean answerCheck() {
+        for (int i = 0; i < gameStatus.length; i++) {
+            if (gameStatus[i].charAt(0) == '_')
+                return false;
+        }
+        return true;
     }
     
 }
